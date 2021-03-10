b7a18355505b1412ffafdf8cc671d65820dbdcfc
umaraniv12 <umaraniv121999@gmail.com>
2021-03-10 10:53:34 +0530

    change6

--- test.go
+++ test.go
@@ -1,18 +1,11 @@
 package main
 
 import "fmt"
-<<<<<<< HEAD
 
 func main() {
 	fmt.Println("hello world")
-	fmt.Println("first local change")
+	fmt.Println("first repo change")
+	fmt.Println("second repo change")
+	fmt.Println("hii all")
+	fmt.Println("third repo change")
 }
-=======
-func main(){
-fmt.Println("hello world")
-  fmt.Println("first repo change")
-  fmt.Println("second repo change")
-  fmt.Println("hii all")
-}
->>>>>>> 3880118af78f951b6ce8826d0d63d1161003f71f
-ex
